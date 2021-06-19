c156c43b0f8dcb9a1979acf60e1161fb857091df
serosi1 <86152730+serosi1@users.noreply.github.com>
2021-06-19 15:40:51 +0300

    Add files via upload

---
+++ fact.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "stdlib.h"
+
+typedef unsigned long long int ull;
+
+ull fact(ull);
+
+int main(int argc, char **argv) {
+	char *end;
+	ull n = strtoull(argv[1], &end, 10);
+	printf("%llu! = %llu\n", n, fact(n)); 
+	return 0;
+}
+
+ull fact(ull n) {
+	if (n == 0) return 1;
+	return fact(n - 1) * n;
+}
